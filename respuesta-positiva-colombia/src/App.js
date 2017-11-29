c47befb8fc11834dc1bfecf78e1d927e1ddef578
Carlos Soto <respuestapositivacolombia@gmail.com>
2017-11-29 18:35:58 -0500

    commit

--- respuesta-positiva-colombia/src/App.js
+++ respuesta-positiva-colombia/src/App.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
+import firebase from 'firebase';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+const config = {
+  apiKey: "AIzaSyC7eFa6_IZn9k5R4n17A5HgGPFdQZCnVrA",
+  authDomain: "respuesta-positiva-f3d86.firebaseapp.com",
+  databaseURL: "https://respuesta-positiva-f3d86.firebaseio.com",
+  projectId: "respuesta-positiva-f3d86",
+  storageBucket: "respuesta-positiva-f3d86.appspot.com",
+  messagingSenderId: "1095933261022"
+};
+
+firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null
+    }
+  }
+
+  componentWillMount() {
+    const nameRef = firebase.database().ref().child('object').child('name');
+    nameRef.on('value', (snapshot) => {
+      this.setState({
+        name: snapshot.val()
+      })
+    })
+  }  
+  
   render() {
     return (
       <div className="App">
@@ -10,9 +40,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">SRF Consultores SAS</h1>
         </header>
-        <p className="App-intro">
-          Bienvenido a Respuesta Positiva
-        </p>
+        <h1>
+          {this.state.name}
+        </h1>
       </div>
     );
   }
