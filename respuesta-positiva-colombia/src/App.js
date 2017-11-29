a2fdafdd001024b0381a02f7e1813426515697b3
Carlos Soto <respuestapositivacolombia@gmail.com>
2017-11-29 18:56:26 -0500

    commit

--- respuesta-positiva-colombia/src/App.js
+++ respuesta-positiva-colombia/src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 import firebase from 'firebase';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { TextField } from 'material-ui';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#273135',
+    primary2Color: '#C0FA32'
+  },
+});
 
 const config = {
   apiKey: "AIzaSyC7eFa6_IZn9k5R4n17A5HgGPFdQZCnVrA",
@@ -20,30 +28,53 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: null
+      name: null,
+      lastname: null
     }
   }
 
   componentWillMount() {
     const nameRef = firebase.database().ref().child('object').child('name');
+    const lastnameRef = firebase.database().ref().child('object').child('lastname');
+    
     nameRef.on('value', (snapshot) => {
       this.setState({
         name: snapshot.val()
       })
     })
-  }  
+
+
+    lastnameRef.on('value', (snapshot) => {
+      this.setState({
+        lastname: snapshot.val()
+      })
+    })
+
+  }
+  
   
   render() {
+    const nameRef = firebase.database().ref().child('object').child('name');
+    const lastnameRef = firebase.database().ref().child('object').child('lastname');
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">SRF Consultores SAS</h1>
-        </header>
-        <h1>
-          {this.state.name}
-        </h1>
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">SRF Consultores SAS</h1>
+          </header>
+          <h1>
+            {this.state.name}
+          </h1>
+          <h1>
+            {this.state.lastname}
+          </h1>
+          <TextField onChange={(e,value)=>{nameRef.set(value)}} type='text' fullWidth={true} floatingLabelFixed={true} floatingLabelText='Mensaje 1' name='mensaje1' hintText='Escribe algo'/>
+          <TextField onChange={(e,value)=>{lastnameRef.set(value)}} type='text' fullWidth={true} floatingLabelFixed={true} floatingLabelText='Mensaje 2' name='mensaje2' hintText='Escribe algo'/>
+          
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
